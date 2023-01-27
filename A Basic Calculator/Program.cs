5f13053a7b1489cf07b9131818a944412a24220d
Bolaji Oluwatofunmi Ashraf <Tofunmibolaji13@gmail.com>
2023-01-27 20:27:50 +0100

    Add project files.

---
+++ A Basic Calculator/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_Basic_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.Write("x = ");
+                string str = Console.ReadLine();
+                if (str == "exit")
+                {
+                    break;
+                }
+                double firstNumber = Convert.ToDouble(str);
+
+                Console.Write("y = ");
+                double secondNumber = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("What operation do you want to do: ");
+                char operationSign = Convert.ToChar(Console.ReadLine());
+
+                switch (operationSign)
+                {
+                    case '+':
+                        Console.WriteLine("your answer is {0}", firstNumber + secondNumber);
+                        break;
+                    case '-':
+                        Console.WriteLine("your answer is {0}", firstNumber - secondNumber);
+                        break;
+                    case '*':
+                        Console.WriteLine("your answer is {0}", firstNumber * secondNumber);
+                        break;
+                    case '/':
+                        Console.WriteLine("your answer is {0}", firstNumber / secondNumber);
+                        break;
+                }
+            }
+            while (true);
+        }
+    }
+}
